0f0b9e74398bb835a5b6a369548b75a4a9b8361d
Ph0enixLeader <77356102+Ph0enixLeader@users.noreply.github.com>
2021-11-22 19:01:52 +0100

    Delete core directory

--- core/php/includes/login.php
+++
@@ -1,5 +0,0 @@
-<?php require '../header.php'; ?>
-
-<link rel="stylesheet" href="../../../../pallium/core/css/default.css">
-<link rel="stylesheet" href="../../../../pallium/core/css/interact.css">
-<?php require '../footer.php'; ?>
