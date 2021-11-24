6cacf29be770959a739fe4f05b8ecb2593fbfb8e
Ph0enixLeader <77356102+Ph0enixLeader@users.noreply.github.com>
2021-11-24 12:42:31 +0100

    Update config.php

--- core/php/includes/db/config.php
+++ core/php/includes/db/config.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $bdd= new PDO('mysql:host=localhost;dbname=pallium;charset=utf8', 'root', '';);
+        $bdd= new PDO('mysql:host=localhost;dbname=pallium;charset=utf8', 'root', '');
     }catch(Exception $e)
     {
         die('Erreur'.$e-getMessage());
