7587f470400be8cbb7387d85c791ebecd6ce005e
Ph0enixLeader <thewatcherfr@gmail.com>
2021-12-01 19:06:49 +0100

    correctif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import About from "./pages/About";
-import Notfound from "./pages/Notfound";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
 import Register from './pages/Register';
+import Testdb from '/pages/Testdb';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/" component={Login} />
         <Route exact path="/about" component={About} />
         <Route exact path="/register" component={Register}/>
+        <Route exact path="/testdb" component={Testdb}/>
         <Route component={Notfound} />
       </Switch>
     </BrowserRouter>
